b75c7ce4ad241fe9a310487d5b5fe4d5c2690288
renanNun <42500047+renanNun@users.noreply.github.com>
2019-05-31 18:22:35 -0300

    Classe Aluno
    Classe Aluno adicionada ao banco

--- models.py
+++ models.py
@@ -28,3 +28,37 @@ class Bolsa(db.Model):
         self.dataFim = dataFim
         self.descricao = clean(descricao)
         self.selecao = clean(selecao)
+
+
+class Aluno(db.Aluno):
+    "Classe de Controle de Alunos registrado na aplicação"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.Text)
+    sobrenome = db.Column (db.Text)
+    telefone = db.Column(db.Text, unique = True)
+    email = db.Column(db.Text, unique = True)
+    nascimento = db.Column(db.DateTime)
+    periodo = db.Column (db.Integer)
+    matricula = db.Column (db.String(8))
+    curso = db.Column(db.Integer)
+    username = db.Column(db.String, unique = True)
+    senha = db.Column(db.String(20))
+    
+    def _init_(self, id, nome,sobrenome,telefone,email,nascimento,periodo,
+               matricula,chave,username,senha):
+        """Constructor"""
+        
+        self.id = clean(id)
+        self.nome = clean(nome)
+        self.sobrenome = clean(sobrenome)
+        self.telefone = clean(telefone)
+        self.email = clean(email)
+        self.nascimento = clean(nascimento)
+        self.periodo = clean(periodo)
+        self.matricula= clean(matricula)
+        self.curso = clean(curso)
+        self.username = clean(username)
+        self.senha = clean(senha)
+    
+    
