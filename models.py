6e68cc975041209105cef68e98068ed373345fe6
Victor Aquiles <victoraqalencar@gmail.com>
2019-06-09 19:59:40 -0300

    Rascunho envio de inscrição para professor

--- models.py
+++ models.py
@@ -2,6 +2,11 @@
 from project import db
 from bleach import clean
 from sqlalchemy import UniqueConstraint
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 class Bolsa(db.Model):
     """Classe modelo para construção da tabela de Bolsas"""
@@ -82,3 +87,70 @@ class InscricaoBolsa(db.Model):
         self.id_bolsa = id_bolsa
         self.data = data
         self.anexo = anexo
+
+        # Enviando email para professor com dados da inscrição
+        self.emailDadosInscricao()
+
+    def emailDadosInscricao(self):
+        port = 465  # For SSL
+        password = 'bolsas@123'
+        password = ''
+
+        sender_email = "bolsasufjf@gmail.com"
+        receiver_email = "bolsasufjf@gmail.com"
+        subject = f"Candidatura da bolsa mamofaf"
+        # body = f"""\
+        # Aluno: {aluno.nome} {aluno.sobrenome}
+        
+        # Matricula: {aluno.matricula}
+
+        # Curso: {aluno.curso}
+
+        # E-mail: {aluno.email}
+        # """
+        body = """\
+        Aluno: aluno.nome} aluno.sobrenome
+        
+        Matricula: aluno.matricula
+
+        Curso: aluno.curso
+
+        E-mail: aluno.email
+        """
+
+        # Create a multipart message and set headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+
+        # Add body to email
+        message.attach(MIMEText(body, "plain"))
+
+        filename = self.anexo
+
+        # Open PDF file in binary mode
+        with open(filename, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+        
+        # Encode file in ASCII characters to send by email    
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename=curriculo.pdf",
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
+
+        # Log in to server using secure context and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, text)
